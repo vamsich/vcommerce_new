39c19f855800e81cdffcedf755b3de96e3f3f35f
vamsi <vamsi.chadaram@gmail.com>
2016-12-19 00:03:05 +0530

    category

--- vcommerce/commercecore/src/main/java/com/commercecore/model/CustomerModel.java
+++ vcommerce/commercecore/src/main/java/com/commercecore/model/CustomerModel.java
@@ -1,27 +1,12 @@
 package com.commercecore.model;
 
-import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="user")
 public class CustomerModel extends UserModel
 {
-	@Column
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private Set<AddressModel> addresses;
-
-	public Set<AddressModel> getAddresses() {
-		return addresses;
-	}
-
-	public void setAddresses(Set<AddressModel> addresses) {
-		this.addresses = addresses;
-	}
+	
 }
